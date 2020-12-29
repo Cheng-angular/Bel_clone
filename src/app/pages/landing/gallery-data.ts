8e8737d91cf735070897415bfb8627746b2b6d7b
Cheng-angular <angulardev0811@outlook.com>
2020-12-29 12:15:38 -0800

    img url fix3

--- src/app/pages/landing/gallery-data.ts
+++ src/app/pages/landing/gallery-data.ts
@@ -4,7 +4,7 @@ export const GalleryData = [
     { id: 2, name: 'domes-6-13', props: 'open space', price: '330', value: 'minilofts', url: '../../../../assets/wp-content/uploads/2020/01/loft-16.jpg' },
     { id: 3, name: 'domes-6-13', props: 'open space', price: '330', value: 'chalets', url: '../../../../assets/wp-content/uploads/2020/01/2-Cortina-9.jpg' },
     { id: 4, name: 'dome shared sauna', props: 'open space', price: '330', value: 'pods', url: '../../../../assets/wp-content/uploads/2020/08/Spa-1.jpg' },
-    { id: 5, name: 'dome private sauna', props:'open space', price: '370', value: 'pods', url: '../../../../assets/wp-content/uploads/2020/08/crépuscule-5.jpg' },
+    { id: 5, name: 'dome private sauna', props:'open space', price: '370', value: 'pods', url: '../../../../assets/wp-content/uploads/2020/08/crepuscule-5.jpg' },
     { id: 6, name: 'mickey domes', props:'open space', price: '360', value: 'pods', url: '../../../../assets/wp-content/uploads/2020/09/03-White-Dome-1.jpg' },
     { id: 7, name: 'twin dome', props:'1 bedroom', price: '370', value: 'pods', url: '../../../../assets/wp-content/uploads/2020/09/DOMOS_FRONTAL_BLANCO-1.jpg' },
     { id: 8, name: 'sky pod', props:'1 bedroom', price: '370', value: 'pods', url: '../../../../assets/wp-content/uploads/2020/08/431830-992277.jpg' },
